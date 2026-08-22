5672473cee2de28df42effed7867e1c7c34fae8f
DelScream <144051024+DelScream@users.noreply.github.com>
2026-08-22 15:44:34 +0000

    Deploying to gh-pages from @ DelScream/sintez-77@accb6a1389facc681fd12a71082701e97bd0b514 🚀

---
+++ assets/page-5xObuzTM.js
@@ -0,0 +1,1 @@
+import{i,a as r}from"./ui-R23jreKS.js";var a;(a=document.querySelector("[data-header]"))==null||a.classList.add("is-scrolled");i();r();
--- assets/page-CS9lddop.js
+++
@@ -1,1 +0,0 @@
-import{i,a as r}from"./ui-Dex5dINC.js";var a;(a=document.querySelector("[data-header]"))==null||a.classList.add("is-scrolled");i();r();
